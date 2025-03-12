75975151f64a1b8f34045f24547c354e4f87e2e0
Zayn1029 <wayne20051029.ii13@nycu.edu.tw>
2025-03-12 18:11:02 +0800

    Add files via upload

---
+++ leetcode1.py
@@ -0,0 +1,61 @@
+class Node:
+    def __init__(self, key=0, value=0):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
+
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {}
+        
+        self.head = Node()
+        self.tail = Node()
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def _remove(self, node):
+        node.prev.next = node.next
+        node.next.prev = node.prev
+
+    def _add(self, node):
+        node.prev = self.head
+        node.next = self.head.next
+        self.head.next.prev = node
+        self.head.next = node
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            node = self.cache[key]
+            self._remove(node)
+            self._add(node)
+            return node.value
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self._remove(self.cache[key])
+            del self.cache[key]
+        
+        node = Node(key, value)
+        
+        self._add(node)
+        self.cache[key] = node
+        
+        if len(self.cache) > self.capacity:
+            lru_node = self.tail.prev
+            self._remove(lru_node)
+            del self.cache[lru_node.key]
+
+# 测试代码
+lru_cache = LRUCache(2)
+lru_cache.put(1, 1)
+lru_cache.put(2, 2)
+print(lru_cache.get(1))
+lru_cache.put(3, 3)
+print(lru_cache.get(2))
+lru_cache.put(4, 4)
+print(lru_cache.get(1))
+print(lru_cache.get(3))
+print(lru_cache.get(4))
